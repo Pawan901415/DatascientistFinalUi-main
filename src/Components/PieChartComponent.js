b35dd90714dba7483dfeac64af4405e657c51f0e
anandbalagar <71076993+anandbalagar@users.noreply.github.com>
2023-12-12 17:41:08 +0530

    Update PieChartComponent.js

--- src/Components/PieChartComponent.js
+++ src/Components/PieChartComponent.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useEffect, useRef, useState, useCallback } from 'react';
 
 const PieChartComponent = () => {
   const chartRef = useRef(null);
